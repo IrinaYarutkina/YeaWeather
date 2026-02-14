ee278ca861581bc1a6ccdde56e8b6e97de1e1ea0
Irina Yarutkina <irinaarutkina606@gmail.com>
2026-02-14 20:07:12 +0300

    Добавлена функция для генерации погоды по часам (на основе текущей температуры);

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //глоб переменные
-
 const apiKey = "ad4bd249c67941033714a1851c8bd49e";
 const clock = document.getElementById("clock");
 const inputCity = document.getElementById("search__input");
@@ -14,8 +13,34 @@ const icon = document.getElementById("weather-card__weather-icon");
 const now = new Date();
 const date = document.getElementById("weather-card__date");
 
+//автоматическая погода для определяемого города по координатам
+//сначала определим коорд --> с пом обратного геокодирования получим город --> получим погоду
+function getUsersCoords() {
+  const MoscowCoord = { lat: 55.75583, lon: 37.6173 }; //по умолчанию
+  console.log(navigator.geolocation);
+  if (!navigator.geolocation) {
+    console.log(
+      "Геолокация не поддерживается, используем координаты по умолчанию"
+    );
+    return MoscowCoord;
+  }
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        let lat = position.coords.latitude;
+        let lon = position.coords.longitude;
+        resolve({ lat, lon });
+        console.log({ lat, lon });
+      },
+      (err) => {
+        console.log("Ошибка:", err.message);
+        resolve(MoscowCoord);
+      }
+    );
+  });
+}
 //____________________________________
-//функция для загрузки погоды для города по поиску
+//функция для загрузки погоды для города
 function getWeather(city) {
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=ru`
@@ -27,8 +52,10 @@ function getWeather(city) {
       return response.json();
     })
     .then((data) => {
+      const currentTemp = Math.round(data.main.temp);
       nameCity.textContent = data.name;
-      temperature.textContent = `${Math.round(data.main.temp)}°C`;
+      temperature.textContent = `${currentTemp}°C`;
+      showWeatherNextHours(currentTemp); //функция для генерации погоды по часам
       icon.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
       humidity.textContent = `${data.main.humidity}%`;
       const pressureMm = Math.round(data.main.pressure * 0.750062);
@@ -49,33 +76,21 @@ function showError() {
   pressure.textContent = "--";
   wind.textContent = "--";
 }
-//вызов функции при нажатии на кнопку
-btnSearch.addEventListener("click", () => {
-  const city = inputCity.value.trim();
-  getWeather(city);
-});
-
 //___________________________
-//реальное время
+//текущее время
 function updateTime() {
   const hours = String(now.getHours()).padStart(2, "0");
   const minutes = String(now.getMinutes()).padStart(2, "0");
-
   clock.textContent = `${hours}:${minutes}`;
 }
 function startClock() {
   updateTime();
-
   const delay = (60 - now.getSeconds()) * 1000;
-
   setTimeout(() => {
     updateTime();
     setInterval(updateTime, 60000);
   }, delay);
 }
-
-startClock();
-
 //переключатель с Цельсия на фаренгейт: изменяет цифровые значения
 const toggle = document.querySelector(".header__switch-toggle");
 const switchWrap = document.querySelector(".header__switch");
@@ -90,7 +105,6 @@ toggle.addEventListener("click", () => {
 //текущая дата
 function updateDate() {
   const today = new Date();
-
   const day = today.getDate().toString().padStart(2, "0");
   const month = (today.getMonth() + 1).toString().padStart(2, "0");
   const year = today.getFullYear();
@@ -98,5 +112,57 @@ function updateDate() {
   date.textContent = `${day}.${month}.${year}`;
 }
 
-updateDate();
-//массив погоды на 12 часов, через каждые 2 часа
+//функция которая показывает погоду через каждые 3 часа
+//1) округляет до ближайшего целого кратного 3 часа, - первая  карточка
+//2) следующие карточки плюс 2(3) часа (00, 03, 06, 09, 12, 15, 18, 21)
+//4)
+function generateTempByHour(hour, nowTemp) {
+  if (hour >= 6 && hour < 18) {
+    let newDayTemp = nowTemp + Math.floor(Math.random() * 5);
+    return newDayTemp;
+  } else {
+    let newNightTemp = nowTemp - Math.floor(Math.random() * 5);
+    return newNightTemp;
+  }
+}
+function showWeatherNextHours(nowTemp) {
+  const hour = now.getHours();
+  let firstCardHour = (Math.ceil(hour / 3) * 3) % 24; //1-карточка
+  let cardsTime = [];
+  for (let i = 0; i < 8; i++) {
+    cardsTime.push((firstCardHour + i * 3) % 24);
+  }
+  console.log(cardsTime);
+
+  cardsTime.forEach((hour) => {
+    const temp = generateTempByHour(hour, nowTemp);
+    console.log(`${hour.toString().padStart(2, "0")}:00`, temp);
+  });
+  return;
+  //функция возвращает разметку(время, изображение рандом, темпа)
+}
+//________________вызовы___________________________
+updateDate(); //обн даты
+startClock(); // для врем
+//вызов функции
+getUsersCoords().then(({ lat, lon }) => {
+  console.log("Используемые координаты:", lat, lon);
+  fetch(
+    `http://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&limit=1&appid=${apiKey}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const nameRu = data[0].local_names.ru;
+      getWeather(nameRu);
+    })
+    .catch((err) => {
+      console.error("Ошибка при получении данных:", err);
+    });
+});
+//вызов функции при нажатии на кнопку: погода по поиску
+btnSearch.addEventListener("click", () => {
+  const city = inputCity.value.trim();
+  getWeather(city);
+});
+
+// showWeatherNextHours(currentTemp);
